06942eb0e63b65b49a6917b6777da93e296f382c
loltempest <owenskippen@gmail.com>
2026-04-01 18:19:48 -0700

    remove: delete dev.bat (confusing, use wsl directly instead)

--- dev.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting pslpro dev servers...
-echo.
-echo Backend server will start on: http://localhost:3000
-echo Frontend will start on: http://localhost:5173
-echo.
-start "pslpro-backend" wsl bash -c "cd '/mnt/c/Users/owens/OneDrive/Documents/Sillyhacks 2026/pslpro' && npm run dev:server"
-start "pslpro-frontend" wsl bash -c "cd '/mnt/c/Users/owens/OneDrive/Documents/Sillyhacks 2026/pslpro' && npm run dev:client"
